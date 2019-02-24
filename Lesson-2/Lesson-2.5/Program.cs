dc11f06e63883bd771b8a5edb5891a98754cfe24
TaoDaK1ng <4itawp@gmail.com>
2019-02-24 06:08:08 +0300

    создал программу imt и подсчёт хороших чиел

---
+++ Lesson-2/Lesson-2.5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2._5
+{
+    class Program
+    {
+        //Чагалысов
+        //Задание 5
+        //а) Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме.
+        //б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+
+        static void Main(string[] args)
+        {
+            double IMT, weight, height, gainKg, loseKg;
+            Console.WriteLine("Введите рост:");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите вес:");
+            weight = Convert.ToDouble(Console.ReadLine()) / 100;
+            IMT = weight / (height * height);
+            gainKg = (18.5 - IMT) * (height * height);
+            loseKg = (IMT - 25) * (height * height);
+            string recomendation = (18.5 > IMT && IMT < 25) ? "У вас нормальный вес." : ((18.5 > IMT) ? $"Нужно набрать {gainKg} кг." : $"Нужно сбросить {loseKg} кг.");
+            Console.WriteLine(recomendation);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson-2/Lesson-2.6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lesson_2._6
+{
+    class Program
+    {
+        //Чагалысов
+        //Задание 6
+        //*Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000. 
+        //«Хорошим» называется число, которое делится на сумму своих цифр.
+        //Реализовать подсчёт времени выполнения программы, используя структуру DateTime.
+        #region Метод подсчета количества цифр числа
+
+        /// <summary>
+        /// Возвращает сумму цифр, заданного числа.
+        /// </summary>
+        /// <param name="number">Число</param>
+        static int SumNumber(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            Thread.Sleep(20);
+            int goodNumber = 0;
+            int countNumber = 0;
+            for (int i = 1; i <= 1000_000_000; i++)
+            {
+                goodNumber = i % SumNumber(i);
+                if (goodNumber == 0) { countNumber++; }
+            }
+            DateTime finish = DateTime.Now;
+            Console.WriteLine($"Количество «хороших» чисел = {countNumber}");
+            Console.WriteLine($"Время выполнения программы: {(finish - start)}");
+            Console.ReadKey();
+        }
+    }
+}
