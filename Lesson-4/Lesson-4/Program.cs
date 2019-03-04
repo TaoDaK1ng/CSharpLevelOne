d3d74f15357f8b7ffb47b8dbcca5b06d17c3258e
TaoDaK1ng <4itawp@gmail.com>
2019-03-04 10:42:02 +0300

    Написал программу по поиску пар в массиве

---
+++ Lesson-4/Lesson-4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    class Program
+    {
+        //Задача 1
+        //Чагалысов
+        //Дан  целочисленный массив  из 20 элементов.Элементы массива  могут принимать  целые значения  от –10 000 до 10 000 включительно.
+        //Заполнить случайными числами.Написать программу, позволяющую найти и вывести количество пар элементов массива,
+        //в которых только одно число делится на 3. В данной задаче под парой подразумевается два подряд идущих элемента массива.
+        //Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2. 
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rand = new Random();
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-10000, 10000);
+                Console.Write($"{array[i]} ");
+            }
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] % 3 == 0 || array[i + 1] % 3 == 0)
+                {
+                    count++;
+                }
+            } 
+            Console.WriteLine("\nКоличество пар: " + count);
+            Console.ReadKey();
+        }
+    }
+}
