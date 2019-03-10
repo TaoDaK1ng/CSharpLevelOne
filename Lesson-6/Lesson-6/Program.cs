f096307b19733309601e021dcd697afb15e3012a
TaoDaK1ng <4itawp@gmail.com>
2019-03-11 02:52:11 +0300

    изменил программу вывода функций

---
+++ Lesson-6/Lesson-6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    //Чагалысов
+    //Задание 1
+    //Изменить программу вывода таблицы функциивывода таблицы функции так,
+    //чтобы можно было передавать функции типа double (double, double).
+    //Продемонстрировать работу на функции с функцией a* x^2 и функцией a* sin(x).
+
+    public delegate double Fun(double x, double a);
+
+    class Program
+    {
+        // Создаем метод, который принимает делегат
+        // На практике этот метод сможет принимать любой метод
+        // с такой же сигнатурой, как у делегата
+        public static void Table(Fun F, double x, double a, double b)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x,a));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+        // Создаем метод для передачи его в качестве параметра в Table
+        public static double MyFuncPow(double x, double a)
+        {
+            return a * Math.Pow(x,2);
+        }
+
+        public static double MyFuncSin(double x, double a)
+        {
+            return a * Math.Sin(x);
+        }
+
+        static void Main()
+        {          
+            Console.WriteLine("Таблица функции a * x^2:");
+            Table(MyFuncPow, -6, 2, 2);      
+            Console.WriteLine("Таблица функции a*sin(x):");
+            Table(MyFuncSin, -6, 2, 2);
+            Console.ReadKey();
+        }
+    }
+}
